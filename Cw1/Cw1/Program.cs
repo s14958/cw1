446e06ace48bd8f42ef494e9a3fefc71d3a65f5b
Hubert Siwkin <hsiwkin@skydistribution.pl>
2020-10-31 18:40:02 +0100

    Add error handling and remove email duplicates

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -10,23 +11,59 @@ namespace Cw1
     {
         static async Task Main(string[] args)
         {
-            string pageUrl = args[0];
-
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(pageUrl);
-
-            if (response.IsSuccessStatusCode)
+            if (args.Length == 0)
             {
-                var html = await response.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z0-9]+@[a-z.]+");
-
-                var matches = regex.Matches(html);
-                foreach(var match in matches)
-                {
-                    Console.WriteLine(match);
-                }
+                throw new ArgumentNullException();
             }
 
+            string pageUrl = args[0];
+            var urlRegex = new Regex("http[s]?://[a-zA-Z.]+");
+            bool isProperUrlString = urlRegex.IsMatch(pageUrl);
+
+            if (!isProperUrlString)
+            {
+                throw new ArgumentException();
+            }
+
+            var httpClient = new HttpClient();
+
+            HttpResponseMessage response;
+            try
+            {
+                response = await httpClient.GetAsync(pageUrl);
+                httpClient.Dispose();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var html = await response.Content.ReadAsStringAsync();
+                    var emailRegex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                    var matches = emailRegex.Matches(html);
+                    
+
+                    if (matches.Count == 0)
+                    {
+                        Console.WriteLine("Nie znaleziono adresów email.");
+                    } else
+                    {
+                        HashSet<string> alreadyPrinted = new HashSet<string>();
+                        foreach (var match in matches)
+                        {
+                            var email = match.ToString();
+                            if (!alreadyPrinted.Contains(email)) 
+                            {
+                                Console.WriteLine(email);
+                                alreadyPrinted.Add(email);
+                            }
+                            
+                        }
+                    }
+                }
+            } catch
+            {
+                Console.WriteLine("Błąd podczas pobierania strony");
+            }
+            
             Console.WriteLine("Koniec!");
         }
     }
